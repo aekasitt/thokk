8aff444d36bc854adabd12ac43e91515b9d0b2bd
Sitt Guruvanich <aekazitt@gmail.com>
2024-12-17 01:00:20 +0700

    chore: no longer ignore build folder

---
+++ dist/thokk.d.ts
@@ -0,0 +1,7 @@
+export declare const thokk: (splits?: number, separator?: string) => string;
+declare const _default: {
+    ADJECTIVES: string[];
+    SURNAMES: string[];
+    thokk: (splits?: number, separator?: string) => string;
+};
+export default _default;
